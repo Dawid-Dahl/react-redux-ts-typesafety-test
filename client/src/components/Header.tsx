eb21792de395876eae205699af5665b05e3c8404
Dawid Dahl <dawiddahl@gmail.com>
2020-02-13 22:01:52 +0100

    reorganized file structure

--- client/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {
-	title: string;
-};
-
-const Header: React.FC<Props> = ({title}) => <div className="header">{title}</div>;
-
-export {Header};
--- server/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-var PORT = 5000;
-app.get("/", function (req, res) {
-    res.send("HELLOES FROM SERVER!");
-});
-app.listen(PORT, function () { return console.log("Server now listening at port: " + PORT); });
