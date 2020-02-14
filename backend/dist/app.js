ee45088f00f6495ad6e1e0693a66cf08cbac6a1a
Dawid Dahl <dawiddahl@gmail.com>
2020-02-14 07:30:54 +0100

    added gitignore file

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -4,10 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var errorhandler = require("errorhandler");
+var morgan = require("morgan");
 var app = express_1.default();
 var PORT = 5000;
-app.get("/", function (req, res) {
-    res.json(666);
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(errorhandler());
+app.get("/api", function (req, res) {
+    res.json(6666);
 });
 app.listen(PORT, function () { return console.log("Server now listening at port: " + PORT); });
 //# sourceMappingURL=app.js.map
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,20 @@
 import express, {Request, Response, Application} from "express";
-var cors = require("cors");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const errorhandler = require("errorhandler");
+const morgan = require("morgan");
 
 const app: Application = express();
 
 const PORT = 5000;
 
+app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan("dev"));
+app.use(errorhandler());
 
 app.get("/api", (req: Request, res: Response) => {
-	res.json(6666);
+	res.json(666);
 });
 
 app.listen(PORT, () => console.log(`Server now listening at port: ${PORT}`));
